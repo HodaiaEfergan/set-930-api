0d79a7eddbda8c074c3b95e29b7a784d45fed572
Hodaia Efergan <hodaia0524289665@gmail.com>
2021-08-05 22:14:50 +0300

    user idan

--- ctrl/usersCtrl.js
+++ ctrl/usersCtrl.js
@@ -18,11 +18,11 @@ module.exports.login = async (req, res) => {
         });
 
         let user = await User.findOne({email: email, password: password}); //search user with this data
-        console.log(user);
-        user.role="owner";
+
         if (!user) return res.status(400).json({success: false, message: 'email or password are incorrect'}); //the name or password is encorrct
         const token = jwt.sign({userId: user._id, email: email}, config.env.JWT_SECRET); //new token
         let answer = user.toObject();
+        answer.role="owner";
         delete answer.password;
         res.json({success: true, data: {token: token, user: answer}});
     } catch (e) {
