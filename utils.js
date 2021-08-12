e0029404c41563e6406346d681a5dfddf3f34b76
Hodaia Efergan <hodaia0524289665@gmail.com>
2021-08-12 15:19:28 +0300

    no nodemailer

--- utils.js
+++ utils.js
@@ -1,24 +1,24 @@
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'gtag930project@gmail.com',
-        pass: '0524289665'
-    }
-});
-
-const mailOptions = {
-    from: 'gtag930project@gmail.com',
-    to: "hodaiaefergan@gmail.com",
-    subject: "subject",
-    text: 'That was easy!'
-};
-transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
-
+// const nodemailer = require("nodemailer");
+//
+// const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     auth: {
+//         user: 'gtag930project@gmail.com',
+//         pass: '0524289665'
+//     }
+// });
+//
+// const mailOptions = {
+//     from: 'gtag930project@gmail.com',
+//     to: "hodaiaefergan@gmail.com",
+//     subject: "subject",
+//     text: 'That was easy!'
+// };
+// transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log('Email sent: ' + info.response);
+//     }
+// });
+//
