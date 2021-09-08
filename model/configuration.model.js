03f1cd10ef1bce0bee9c2358acd622fdc7bb2b55
HodaiaEfergan <62573380+HodaiaEfergan@users.noreply.github.com>
2021-09-08 21:25:23 +0300

    Update configuration.model.js

--- model/configuration.model.js
+++ model/configuration.model.js
@@ -28,6 +28,14 @@ const schema = new mongoose.Schema({
             value: {type: Number, default: 20},
         }
     },
+    wifiConnection: {
+        type:{
+            enabled: {type: Boolean, default: false},
+            name: {type:String , default: ''},
+            password: {type:String , default: ''}
+
+        }
+    },
 
     alertMethods: {
         type: {
