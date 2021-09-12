9562d23d314f84aec9c0f6ee60b713328a24dc0a
Hodaia Efergan <hodaia0524289665@gmail.com>
2021-09-12 12:58:22 +0300

    new commit

--- ctrl/unitCtrl.js
+++ ctrl/unitCtrl.js
@@ -101,7 +101,7 @@ async function handleConfiguration(unit, scanData) {
 
     // load configuration from server
     let config = unit.configuration;
-    //if (!config.enabled) return;
+    if (!config.enabled) return;
     //if (!config.alertMethods.sms.enabled && !config.alertMethods.email.enabled) return;
 
     // check cpu temp - config.alertMethods.email.email
